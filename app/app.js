225ed4544c8ac9ab449841f2d42d063047fa5a95
Mateusz Matusiak <mateusz.matusiak@ardentcode.com>
2021-12-16 10:09:21 +0100

    to cache or not to cache

--- app/app.js
+++ app/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-   res.send('hello world');
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/not-cached', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 'no-store');
+    res.send(JSON.stringify({hello: 'world'}));
+});
+
+app.get('/cached', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 'public, max-age=60');
+    res.send(JSON.stringify({hello: 'cached world'}));
 });
 
 app.listen(port, () => {
